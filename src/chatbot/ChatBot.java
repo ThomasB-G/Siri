41c3ef55ca13dff17e41dbc745b937d98b88f779
russell <russell@104.39.80.195>
2018-04-12 14:10:58 -0400

    Added interaction to main

--- src/chatbot/ChatBot.java
+++ src/chatbot/ChatBot.java
@@ -16,6 +16,7 @@ public class ChatBot {
      */
     public static void main(String[] args) {
         System.out.println("Hello World");
+        Interaction interaction = new Interaction();
     }
     
 }
